44771eaa3dab9faf65b029f323a7a427fa5273d3
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-05-20 00:50:22 +0300

    corect css
    isOnline don't work(

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -7,12 +7,12 @@ export function Profile(props) {
     tag,
     location,
     avatar,
-    stats: { followers, views, likes },
+    stats,
   } = props;
   return (
     <div className="profile">
       <div className="description">
-        <img src={avatar} alt="User avatar" className="avatar" />
+        <img src={avatar} alt="User avatar" className="avatar avatar_profile" />
         <p className="name">{username}</p>
         <p className="tag">@{tag}</p>
         <p className="location">{location}</p>
@@ -21,30 +21,30 @@ export function Profile(props) {
       <ul className="stats">
         <li>
           <span className="label">Followers</span>
-          <span className="quantity">{followers}</span>
+          <span className="quantity">{stats.followers}</span>
         </li>
         <li>
           <span className="label">Vievs</span>
-          <span className="quantity">{views}</span>
+          <span className="quantity">{stats.views}</span>
         </li>
         <li>
           <span className="label">Likes</span>
-          <span className="quantity">{likes}</span>
+          <span className="quantity">{stats.likes}</span>
         </li>
       </ul>
     </div>
   );
 }
 
+
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   avatar: PropTypes.string.isRequired,
-  stats: PropTypes.objectOf(
-    PropTypes.exact({
-      label: PropTypes.string.isRequired,
-      quantity: PropTypes.number.isRequired,
-    })
-  ),
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
 };
