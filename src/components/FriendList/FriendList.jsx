5f75dd83492f962399bec1c3aa67c235a3e645e3
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-05-21 17:34:54 +0300

    added some things

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import css from './FriendList.module.css';
 import { FriendListItem } from '../FriendListItem/FriendListItem';
 
-export function FriendList ({ friends }) {
+export function FriendList({ friends }) {
   const friendList = friends.map(friend => (
     <FriendListItem
       key={friend.id}
@@ -10,21 +10,16 @@ export function FriendList ({ friends }) {
       name={friend.name}
       isOnline={friend.isOnline}
     />
-  )
-  )
-  return (
-    <ul className={css.friend_list}>
-      {friendList}
-    </ul>
-  );
+  ));
+  return <ul className={css.friend_list}>{friendList}</ul>;
 }
 
 FriendList.propTypes = {
-    friends: PropTypes.arrayOf(
-        PropTypes.shape({
-            id: PropTypes.number.isRequired,
-            avatar: PropTypes.string.isRequired,
-            name: PropTypes.string.isRequired,
-        })
-    )
-}
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+    })
+  ),
+};
