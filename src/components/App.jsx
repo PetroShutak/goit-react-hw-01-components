d47f98ad58dac636b59cedb6e1288fa1289d2f58
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-05-25 13:00:49 +0300

    added footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,6 +2,7 @@ import { Profile } from './Profile/Profile';
 import { Statistics } from './Statistics/Statistics';
 import { FriendList } from './FriendList/FriendList';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
+import { Footer } from './Footer/Footer';
 import user from './data/user.json';
 import data from './data/data.json';
 import friends from './data/friends.json';
@@ -25,6 +26,7 @@ export const App = () => {
         <FriendList friends={friends} />
         <h1>"TransactionHistory"</h1>
         <TransactionHistory items={transactions} />
+        <Footer />
       </div>
     </div>
   );
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,18 +2,20 @@ import PropTypes from 'prop-types';
 import css from './FriendListItem.module.css';
 
 export function FriendListItem({ avatar, name, isOnline }) {
-    const itemClassName = isOnline ? `${css.item} ${css.online}` : `${css.item} ${css.offline}`;
-    return (
-        <li className={itemClassName}>
-           <span className={isOnline ? css.statusOnline : css.statusOffline} />
-            <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
-            <p className={css.name}>{name}</p>
-        </li>
-    );
+  const itemClassName = isOnline
+    ? `${css.item} ${css.online}`
+    : `${css.item} ${css.offline}`;
+  return (
+    <li className={itemClassName}>
+      <span className={isOnline ? css.statusOnline : css.statusOffline} />
+      <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+      <p className={css.name}>{name}</p>
+    </li>
+  );
 }
 
 FriendListItem.propTypes = {
-    avatar: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    isOnline: PropTypes.bool.isRequired,
-}
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  isOnline: PropTypes.bool.isRequired,
+};
